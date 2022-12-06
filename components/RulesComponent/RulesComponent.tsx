18df7040b85b57b8a5cc940f969057022cd8a139
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-12-06 10:32:27 +0100

    adjust rules display

--- components/RulesComponent/RulesComponent.tsx
+++ components/RulesComponent/RulesComponent.tsx
@@ -10,9 +10,9 @@ const RulesComponent: React.FC<{
         <div className="fixed overflow-y-auto bg-darkBackground z-10 top-0 h-screen w-screen flex flex-col items-center  rounded-lg  ">
           <div
             onClick={() => setShowRules(false)}
-            className="fixed overflow-y-auto bg-white  z-10 top-0 h-screen w-screen flex flex-col items-center sm:h-[60%] sm:w-[60%] sm:top-[25%] sm:max-w-[560px] sm:max-h-[560px] rounded-lg  "
+            className="fixed overflow-y-auto bg-white  z-10 top-0 h-screen w-screen flex flex-col items-center sm:h-[60%] sm:w-[60%] sm:top-[25%] sm:max-w-[600px] sm:max-h-[700px] pb-4 rounded-lg  "
           >
-            <div className="flex w-full justify-between items-center p-8 ">
+            <div className="flex w-full justify-between items-center p-8 pb-24 sm:pb-2 ">
               <div className="text-darkText text-3xl text-center w-full sm:text-left ">
                 RULES
               </div>
@@ -23,7 +23,7 @@ const RulesComponent: React.FC<{
                 <IconClose />
               </div>
             </div>
-            <div className="z-10 rulesBg w-full h-full"></div>
+            <div className="z-10 rulesBg w-[60%] sm:w-[70%] h-[40%] sm:h-[90%]"></div>
           </div>
         </div>
       ) : null}
